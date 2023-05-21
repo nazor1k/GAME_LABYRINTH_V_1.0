769fde5ce1739ced7a8680968d43360ac97e9c8c
nazor1k <118628199+nazor1k@users.noreply.github.com>
2023-05-21 15:29:58 +0300

    оголошення функцій

--- GAME/main_head.h
+++ GAME/main_head.h
@@ -1,10 +1,12 @@
-//
-// Created by Nazar on 20.05.2023.
-//
+
 
 //#ifndef GAME_LABYRINTH_V_1_0_MAIN_HEAD_H
 //#define GAME_LABYRINTH_V_1_0_MAIN_HEAD_H
 //
 //#endif //GAME_LABYRINTH_V_1_0_MAIN_HEAD_H
 #pragma once
-int func(int a,int b);
+void add_score_points(int w_h[3][2], char* map[],int num);
+void drawMaze(char us_in, bool* collect, int w_h[3][2], char* map[]);
+void movePlayer(char userInput, int w_h[3][2], char* map[]);
+void collect_key_or_exit(bool* c_k,int w_h[3][2],char* map[]);
+void game();
