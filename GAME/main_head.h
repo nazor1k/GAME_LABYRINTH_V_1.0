0e8be93dc87eb3aec40d04244935b5fd563e03dc
nazor1k <118628199+nazor1k@users.noreply.github.com>
2023-05-21 23:34:31 +0300

    доробив майже все окрім інформації

--- GAME/main_head.h
+++ GAME/main_head.h
@@ -5,8 +5,15 @@
 //
 //#endif //GAME_LABYRINTH_V_1_0_MAIN_HEAD_H
 #pragma once
+void enter_to_action();
+char player_direction(char us_in);
+void START_func(char us_in, bool* collect, int w_h[3][2], char* map[]);
+void LEVELS_func(char us_in, bool* collect, int w_h[3][2], char* map[]);
+void INFO_func();
+void main_menu();
+void print_end_stats(int start, int end);
 void add_score_points(int w_h[3][2], char* map[],int num);
 void drawMaze(char us_in, bool* collect, int w_h[3][2], char* map[]);
 void movePlayer(char userInput, int w_h[3][2], char* map[]);
-void collect_key_or_exit(bool* c_k,int w_h[3][2],char* map[]);
+void collect_key_or_exit_or_teleport_or_point(bool* c_k,int w_h[3][2],char* map[]);
 void game();
